e9b6a766356c52ec6f38488fb9be10f7ee118bc1
Yukai Huang <yukaihuangtw@gmail.com>
2020-07-18 17:14:43 +0800

    Change mongo db environement variable name

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb'
 
-const client = new MongoClient(process.env.MONGODB_URL, {
+const client = new MongoClient(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
