00a36e2f1b43500a7270078ed4411e9ad2879ee4
Yukai Huang <yukaihuangtw@gmail.com>
2021-01-04 20:32:06 +0800

    Add coupon tab

--- pages/orders.js
+++ pages/orders.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect, useContext } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import cx from 'classnames'
 
 import { orderStatusName } from '../common/order'
 import { UserContext } from '../components/UserContext'
@@ -10,6 +11,9 @@ const Orders = () => {
   const [, setOrderId] = useState(null)
   const [orders, setOrders] = useState([])
   const { user } = useContext(UserContext)
+  const [tab, setTab] = useState('order')
+  const isOrderTab = tab === 'order'
+  const isCouponTab = tab === 'coupon'
 
   useEffect(() => {
     if (!user) {
@@ -38,35 +42,46 @@ const Orders = () => {
   return (
     <div className='page-container'>
       <Head>
-        <title>所有訂單</title>
+        <title>我的訂單</title>
       </Head>
 
       <div className='container'>
+        <div className='tabs'>
+          <nav className='nav nav-tabs nav-justified'>
+            <a className={cx("nav-item nav-link", { active: isOrderTab })} onClick={() => setTab('order')}>檢視訂單</a>
+            <a className={cx("nav-item nav-link", { active: isCouponTab })} onClick={() => setTab('coupon')}>檢視優惠券</a>
+          </nav>
+        </div>
+
         <div className='table table-striped table-responsive'>
-          <table className='table'>
-            <thead>
-              <tr>
-                <th>訂單編號</th>
-                <th>金額</th>
-                <th>狀態</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                orders.map(order => (
-                  <tr key={order.id}>
-                    <td>
-                      <Link href='/orders/[id]' as={`/orders/${order.legacyResourceId}`}>
-                        <a>{order.legacyResourceId}</a>
-                      </Link>
-                    </td>
-                    <td>NT$ {order.totalPrice}</td>
-                    <td>{orderStatusName[order.status]}</td>
+          {
+            tab === 'order' ? <table className='table'>
+                <thead>
+                  <tr>
+                    <th>訂單編號</th>
+                    <th>金額</th>
+                    <th>狀態</th>
                   </tr>
-                ))
-              }
-            </tbody>
-          </table>
+                </thead>
+                <tbody>
+                  {
+                    orders.map(order => (
+                      <tr key={order.id}>
+                        <td>
+                          <Link href='/orders/[id]' as={`/orders/${order.legacyResourceId}`}>
+                            <a>{order.legacyResourceId}</a>
+                          </Link>
+                        </td>
+                        <td>NT$ {order.totalPrice}</td>
+                        <td>{orderStatusName[order.status]}</td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </table> : <table className='table'>
+
+            </table>
+          }
         </div>
       </div>
     </div>
