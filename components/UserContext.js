7e1bc82f90e845091bc1b8e0896777a8d7c0d24b
Yukai Huang <yukaihuangtw@gmail.com>
2021-01-08 16:21:44 +0800

    Reflow liff login

--- components/UserContext.js
+++ components/UserContext.js
@@ -1,5 +1,5 @@
-import React, { createContext, useState, useEffect } from 'react'
-import { isDuringLiffRedirect } from '../utils/liff'
+import React, { createContext, useEffect, useState } from 'react'
+import { isDuringLiffRedirect, useLiffClient } from '../utils/liff'
 
 /**
  * @typedef LiffState
@@ -14,31 +14,45 @@ import { isDuringLiffRedirect } from '../utils/liff'
  * @property {import('@line/liff').default} liff
  * @property {any} user
  * @property {LiffState} liffState
- * @property {any} getLineProfile
  */
 
 /** @type {import('react').Context<TUserContext>} */
 export const UserContext = createContext({
   liff: null,
   user: null,
-  liffState: null,
-  getLineProfile: null
+  liffState: null
 })
 
 export default UserContext
 
 export const withUserContext = Components => (props) => {
-  /** @type [import('@line/liff').default, any] */
-  const [liffClient, setLiffClient] = useState(null)
+  const [lineProfile, setLineProfile] = useState(null)
   const [liffState, setLiffState] = useState({
     loaded: false,
     isInClient: false,
     isLoggedIn: false,
     profile: null
   })
-
   const [user, setUser] = useState(null)
 
+  // init liff client
+  const [liffClient] = useLiffClient(async liff => {
+    await initializeLiffClient(liff)
+  })
+
+  useEffect(() => {
+    if (!user && lineProfile && liffClient) {
+      doLogin(liffClient)
+    }
+  }, [
+    lineProfile,
+    liffClient,
+    user
+  ])
+
+  /**
+   * @param {import('@line/liff').default} liff
+   */
   const initializeLiffClient = async (liff) => {
     let liffId, redirectUri
     if (window.location.pathname === '/liff') {
@@ -62,68 +76,46 @@ export const withUserContext = Components => (props) => {
       loaded: true
     })
 
-    let profile
     if (liff.isLoggedIn()) {
-      profile = await liff.getProfile()
+      setLineProfile(await liff.getProfile())
     } else {
       // TODO: handle dynamic path redirection
-      await liff.login({
+      return await liff.login({
         redirectUri
       })
     }
+  }
 
-    window.fetch('/api/login', {
+  const doLogin = async (liff) => {
+    const data = await window.fetch('/api/login', {
       method: 'POST',
       body: JSON.stringify({
-        profile
+        profile: lineProfile
       }),
       headers: {
         'Content-Type': 'application/json'
       }
     }).then(res => res.json())
-      .then(data => {
-        if (data.status === 'ok') {
-          const { data: { user } } = data
 
-          setUser(user)
+    if (data.status === 'ok') {
+      const { data: { user } } = data
 
-          setLiffState({
-            isInClient: liff.isInClient(),
-            isLoggedIn: liff.isLoggedIn(),
-            loaded: true,
-            profile
-          })
-        }
+      setUser(user)
+
+      setLiffState({
+        isInClient: liff.isInClient(),
+        isLoggedIn: liff.isLoggedIn(),
+        loaded: true,
+        profile: lineProfile
       })
-  }
-
-  useEffect(() => {
-    import('@line/liff').then(liff => {
-      setLiffClient(liff)
-      initializeLiffClient(liff)
-    })
-  }, [])
-
-  const getLineProfile = (user) => {
-    const metafields = user && user.metafields?.edges
-    if (!metafields) {
-      return null
     }
-
-    const metafield = metafields.find(m => m.node.key === 'line_profile')
-    if (!metafield) {
-      return null
-    }
-
-    return JSON.parse(metafield.node.value)
   }
 
   return (
     <UserContext.Provider value={{
       liff: liffClient,
       user,
-      liffState,
-      getLineProfile
+      liffState
     }}
     >
       <Components {...props} />
--- utils/liff.js
+++ utils/liff.js
@@ -1,4 +1,29 @@
+import { useEffect, useState } from 'react'
+
 export const isDuringLiffRedirect = () => {
   const params = new URLSearchParams(location.search)
   return !!params.get('liff.state')
+}
+
+/**
+ * @callback useLiffClientCallback
+ * @param {import('@line/liff').default} liff
+ */
+
+/**
+ *
+ * @param {useLiffClientCallback} cb
+ */
+export const useLiffClient = (cb) => {
+  /** @type [import('@line/liff').default, any] */
+  const [liffClient, setLiffClient] = useState(null)
+
+  useEffect(() => {
+    import('@line/liff').then(liff => {
+      setLiffClient(liff)
+      cb(liff)
+    })
+  }, [])
+
+  return [liffClient]
 }
