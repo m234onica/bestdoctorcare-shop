09ee11665f871f07ba97df2cbe38cf87edcf0d83
joycehsu <joycehsu@stareksci.com>
2021-06-14 22:49:42 +0800

    style: notification's UI

--- pages/notification.vue
+++ pages/notification.vue
@@ -1,19 +1,72 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+    <div>
+        <v-row align="center">
+            <v-col class="d-flex" cols="12" sm="4">
+                <v-select v-model="customer" :items="customerItems" label="使用者" dense></v-select>
+            </v-col>
+
+            <v-col cols="12" sm="4">
+                <v-menu
+                    ref="menu"
+                    v-model="menu"
+                    :close-on-content-click="false"
+                    :return-value.sync="dates"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="auto"
+                >
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-combobox
+                            v-model="dates"
+                            multiple
+                            chips
+                            small-chips
+                            label="日期範圍"
+                            prepend-icon="mdi-calendar"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                        ></v-combobox>
+                    </template>
+                    <v-date-picker v-model="dates" multiple no-title scrollable>
+                        <v-spacer></v-spacer>
+                        <v-btn text color="primary" @click="menu = false"> Cancel </v-btn>
+                        <v-btn text color="primary" @click="$refs.menu.save(dates)"> OK </v-btn>
+                    </v-date-picker>
+                </v-menu>
+            </v-col>
+
+            <v-col class="d-flex" cols="12" sm="4">
+                <v-select v-model="tags" :items="tagItems" label="商品種類" dense></v-select>
+            </v-col>
+        </v-row>
+        <v-textarea outlined name="input-7-4" label="通知內容" value="" class="mb-0 pb-0" :messages="message"></v-textarea>
+        <v-row>
+            <v-spacer></v-spacer>
+            <v-btn color="primary">送出</v-btn>
+        </v-row>
+    </div>
 </template>
+<script>
+export default {
+    data: () => ({
+        customer: "所有人",
+        customerItems: ["所有人", "購買", "檢視"],
+        tags: "全部",
+        tagItems: ["全部", "保健食品類"],
+        dates: [],
+        menu: false,
+        msgCount: 0,
+        totalMsgCount: 0,
+        message: "",
+    }),
+    mounted() {
+        this.message = "則數提醒：目前共 " + this.msgCount + " 則，發送給  人，共計送出 " + this.totalMsgCount + " 則";
+    },
+};
+</script>
+<style lang="scss">
+    .spacer {
+        flex-grow: 0.989 !important;
+    }
+</style>
