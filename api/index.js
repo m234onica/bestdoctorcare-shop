4c2c2f40482dd6853fe8a3ec293d0547c3418ce1
joycehsu <joycehsu@stareksci.com>
2021-06-19 11:41:28 +0800

    feat: finish announcement's created api

--- api/index.js
+++ api/index.js
@@ -1,11 +1,30 @@
 // index.js
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
+import bodyParser from "body-parser";
 
 const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get(`/posts`, async (req, res) => {
+    const result = await prisma.announcement.findMany({});
+    res.json(result);
+})
+
+app.post(`/announce/post`, async (req, res) => {
+    const result = await prisma.announcement.create({
+        data: {
+            title: req.body.data.title,
+            content: req.body.data.content
+        }
+    })
+    res.status(200).json(result);
+})
 
 app.get(`/discounts`, async (req, res) => {
     const result = await prisma.discount.findMany({});
--- pages/announcement.vue
+++ pages/announcement.vue
@@ -1,38 +1,59 @@
 <template>
-    <v-simple-table fixed-header height="300px">
-        <template v-slot:default>
-            <thead>
-                <tr>
-                    <th class="text-left">標題</th>
-                    <th class="text-left">發布時間</th>
-                    <th class="text-left">動作</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="item in desserts" :key="item.name">
-                    <td>{{ item.name }}</td>
-                    <td>{{ item.createdAt }}</td>
-                    <td>
-                        <v-btn color="primary">編輯</v-btn>
-                        <v-btn color="error">刪除</v-btn>
-                    </td>
-                </tr>
-            </tbody>
-        </template>
-    </v-simple-table>
+    <v-container>
+        <div id="announcement">
+            <h1 fixed>公告管理</h1>
+            <v-row>
+                <v-spacer></v-spacer>
+                <template>
+                    <v-btn color="primary" class="mx-15" @click.stop="addDialog = true">新增公告</v-btn>
+                </template>
+                <add :addDialog="addDialog" @close="close"></add>
+            </v-row>
+            <v-simple-table fixed-header class="mt-8" height="300px">
+                <template v-slot:default>
+                    <thead>
+                        <tr>
+                            <th class="text-left">標題</th>
+                            <th class="text-left">發布時間</th>
+                            <th class="text-center">動作</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="item in data" :key="item.id">
+                            <td>{{ item.title }}</td>
+                            <td>{{ item.createdAt }}</td>
+                            <td class="text-center">
+                                <v-btn color="secondary">編輯</v-btn>
+                                <v-btn color="error">刪除</v-btn>
+                            </td>
+                        </tr>
+                    </tbody>
+                </template>
+            </v-simple-table>
+        </div>
+    </v-container>
 </template>
 
 <script>
+import add from "../components/announce/add.vue";
 export default {
+    components: {
+        add,
+    },
+    async asyncData({ $axios }) {
+        const data = await $axios.$get(`${process.env.APP_URL}/posts`);
+        return { data: data };
+    },
+    middleware: "token",
     data() {
         return {
-            desserts: [
-                {
-                    name: "測試",
-                    createdAt: "測試測試",
-                },
-            ],
+            addDialog: false,
         };
+    },
+    methods: {
+        close() {
+            this.addDialog = false;
+        },
     },
 };
 </script>
