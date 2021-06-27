3fcd36c7f628f96a39aa6513aa645e327a28e7d5
Yukai Huang <yukaihuangtw@gmail.com>
2021-06-27 12:05:15 +0800

    Add EventLog table

---
+++ prisma/migrations/20210702121551_add_event_log/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `EventLog` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `type` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191),
+    `payload` JSON,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `EventLog.type_index`(`type`),
+    INDEX `EventLog.userId_index`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
