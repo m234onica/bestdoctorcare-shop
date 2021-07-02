d94451c143c9b28f92e8d588a96b5493daf764c8
Yukai Huang <yukaihuangtw@gmail.com>
2021-07-02 20:34:21 +0800

    Add productId to eventLog

--- prisma/migrations/20210702123303_add_event_log/migration.sql
+++ prisma/migrations/20210702123303_add_event_log/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE `EventLog` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `type` VARCHAR(191) NOT NULL,
+    `productId` VARCHAR(191),
     `userId` VARCHAR(191),
     `payload` JSON,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -9,5 +10,6 @@ CREATE TABLE `EventLog` (
 
     INDEX `EventLog.type_index`(`type`),
     INDEX `EventLog.userId_index`(`userId`),
+    INDEX `EventLog.productId_index`(`productId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
