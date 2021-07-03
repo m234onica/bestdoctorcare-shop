c0c0c0c3fc9636d4501e4b774fabf0f05393906a
joycehsu <joycehsu@stareksci.com>
2021-07-03 18:55:48 +0800

    feat: add announcement to root path

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<!-- @format -->
+
+<template></template>
+
+<script>
+export default {
+    components: {},
+    async asyncData({ redirect }) {
+        redirect(302, "/announcement");
+    },
+};
+</script>
