e3c8189fe7363e9f03d59048b1597d23f2f3bc13
joycehsu <joycehsu@stareksci.com>
2021-07-10 16:45:43 +0800

    feat: send line message

--- api/routes/notification.js
+++ api/routes/notification.js
@@ -1,17 +1,25 @@
+import { RequestError } from "@line/bot-sdk";
 import { PrismaClient } from "@prisma/client";
+import { response } from "express";
 var express = require("express");
 var Shopify = require('shopify-api-node');
+var Line = require('@line/bot-sdk');
 
 var router = express.Router();
 var prisma = new PrismaClient();
-var shopify = new Shopify({
+
+const shopify = new Shopify({
     shopName: process.env.SHOPIFY_API_URL,
     apiKey: process.env.SHOPIFY_API_KEY,
     password: process.env.SHOPIFY_ACCESS_TOKEN
 });
 
+const client = new Line.Client({
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_CHANNEL_SECRET
+});
+
 router.get(`/collection`, async (req, res) => {
-    const collectionFields = ["handle"].join(',');
     await shopify.collectionListing.list({
         limit: 30,
         fields: ["handle"]
@@ -20,4 +28,32 @@ router.get(`/collection`, async (req, res) => {
     })
 });
 
+router.get(`/lineId`, async (req, res) => {
+    const result = await prisma.shopifyUserLineUserRelation.findMany();
+    res.send(result);
+})
+
+router.post(`/sendMessage`, async (req, res) => {
+    var content = req.body.content;
+    var lineIdArry = req.body.lineIdArry;
+    var status = 200;
+    if (content.length == 0) {
+        status = 404;
+        response.message = "通知內容為必填！";
+    } else if (lineIdArry.length == 0) {
+        status = 404;
+        response.message = "沒有可發送的客戶！";
+    } else {
+        client.multicast(lineIdArry, { type: "text", text: content })
+            .then(() => {
+                response.message = "發送成功，共發送了" + lineIdArry.length + "則通知！";
+            })
+            .catch((err) => {
+                if (err instanceof Line.RequestError || err instanceof Line.HTTPError) {
+                    res.status(err.statusCode).json(err.message);
+                }
+            });
+    };
+    res.status(status).json(response);
+})
 module.exports = router;
--- pages/notification/index.vue
+++ pages/notification/index.vue
@@ -1,12 +1,12 @@
 <template>
     <v-container>
         <div id="notification">
+            <v-alert :color="type" v-if="type" dense text>{{ responseMsg }}</v-alert>
             <h1 fixed>發送通知</h1>
             <v-row align="center" class="mt-8">
                 <v-col class="d-flex" cols="12" sm="4">
-                    <v-select v-model="customer" :items="customerItems" label="使用者" dense></v-select>
+                    <v-select v-model="customer" :items="customerItems" label="使用者" @change="getLineIdArry(customer)" dense></v-select>
                 </v-col>
-
                 <v-col cols="12" sm="4">
                     <v-menu
                         ref="menu"
@@ -43,10 +43,21 @@
                     <v-select v-model="defaultCollection" :items="collectionsItems" label="商品種類" dense :disabled="disabled"></v-select>
                 </v-col>
             </v-row>
-            <v-textarea outlined name="input-7-4" label="通知內容" value="" class="mb-0 pb-0" :messages="message"></v-textarea>
+            <span id="msg_count">則數提醒：目前共 {{ msgCount }} 則，發送給 {{ lineIdCount }} 人，共計送出 {{ totalMsgCount }} 則</span>
+            <v-textarea
+                outlined
+                name="input-7-4"
+                label="通知內容"
+                v-model="content"
+                class="mb-0 pb-0"
+                :error="notifyFail"
+                :messages="errorMsg"
+                @blur="clearError"
+            ></v-textarea>
+
             <v-row>
                 <v-spacer></v-spacer>
-                <v-btn color="primary">送出</v-btn>
+                <v-btn color="primary" @click="submit">送出</v-btn>
             </v-row>
         </div>
     </v-container>
@@ -62,15 +73,19 @@ export default {
         }
     },
     data: () => ({
+        type: null,
         customer: "所有人",
         customerItems: ["所有人", "購買", "檢視"],
         defaultCollection: "全部",
         collectionsItems: ["全部"],
         dates: [],
         menu: false,
-        msgCount: 0,
-        totalMsgCount: 0,
-        message: "",
+        content: "",
+        errorMsg: "",
+        notifyFail: false,
+        msgCount: 1,
+        lineIdArry: [],
+        responseMsg: null,
     }),
     computed: {
         disabled() {
@@ -80,17 +95,72 @@ export default {
             }
             return disabled;
         },
+        lineIdCount() {
+            return this.lineIdArry.length;
+        },
+        totalMsgCount() {
+            return this.lineIdCount * this.msgCount;
+        },
     },
     mounted() {
         this.collections.forEach((item) => {
             this.collectionsItems.push(item.handle);
         });
-        this.message = "則數提醒：目前共 " + this.msgCount + " 則，發送給  人，共計送出 " + this.totalMsgCount + " 則";
+        this.getLineIdArry(this.customer);
+    },
+    methods: {
+        getLineIdArry(customer) {
+            let $vm = this;
+            $vm.lineIdArry = [];
+            if (customer == "所有人") {
+                $vm.$axios.get("/lineId").then(function (response) {
+                    response.data.forEach((item) => {
+                        $vm.lineIdArry.push(item.lineUserId);
+                    });
+                });
+            } else if (customer == "檢視") {
+            } else {
+            }
+        },
+        clearError() {
+            this.errorMsg = "";
+            this.notifyFail = false;
+        },
+        submit() {
+            let $vm = this;
+            if ($vm.content.length != 0) {
+                $vm.$axios
+                    .post("/sendMessage", {
+                        lineIdArry: $vm.lineIdArry,
+                        content: $vm.$data.content,
+                    })
+                    .then((response) => {
+                        this.type = "success";
+                        this.responseMsg = response.data.message;
+                    })
+                    .catch((error) => {
+                        this.type = "error";
+                        console.log(error);
+                        this.responseMsg = error.response.data.message;
+                    });
+            } else {
+                $vm.errorMsg = "通知內容為必填";
+                $vm.notifyFail = true;
+            }
+        },
     },
 };
 </script>
 <style lang="scss">
 .spacer {
     flex-grow: 0.989 !important;
+}
+#msg_count {
+    font-size: 14px;
+    color: gray;
+}
+#error_msg {
+    display: block;
+    color: red;
 }
 </style>
