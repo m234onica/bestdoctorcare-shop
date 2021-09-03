05c5108c6bfcc54956bf9f5e51de6027b0370dff
joycehsu <joycehsu@stareksci.com>
2021-09-03 11:55:39 +0800

    refactor: meta name

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
         titleTemplate: '%s - bestdoctorcare-admin',
-        title: 'bestdoctorcare-admin',
+        title: '百漢中醫管理後台',
         htmlAttrs: {
             lang: 'en'
         },
